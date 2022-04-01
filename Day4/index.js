c00c98302501a7ecf4c244dfac6433e266f4b345
Suraj <rauniyar.suraj731@gmail.com>
2022-04-01 18:18:28 +0530

    day 4

--- Day4/index.js
+++ Day4/index.js
@@ -1,3 +1,15 @@
-var m = 1 
+const express = require("express")
 
-bag  += m++ + " ";
+const app = express();
+
+app.listen(1000,()=>{
+    console.log("listening on port 1000")
+})
+const allbooks = (req,res,next)=>{
+    console.log("Fetching All Books"),
+    next();
+}
+
+app.get("/books",allbooks,(req,res)=>{
+    return res.send("i m a book")
+})
