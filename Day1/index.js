9e4ed7275cad5612a98392b8e51e337ff33ae8a3
Suraj <rauniyar.suraj731@gmail.com>
2022-04-03 20:50:42 +0530

    updated

--- Day1/index.js
+++ Day1/index.js
@@ -4,16 +4,16 @@
 
  app.get("/",function(req,res){
      res.send("pablo")
- })
+ });
 
- app.listen(3300, ()=>{
+ app.listen(3000, ()=>{
     console.log("object");
-})
+});
 
  app.get("/books", function(req,res){
-       res.send("Dream with open eyes","Wings of Fire","The Psychology of Money,Dairy of young Girl")
+       res.send("Dream with open eyes","Wings of Fire","The Psychology of Money,Dairy of young Girl");
  })
 
  app.listen(2800, ()=>{
      console.log("object1");
- })
+ });
